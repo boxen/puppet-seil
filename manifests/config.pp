d4f9ab4ffd0114eb04eb0a726512c6265a118516
bryan kennedy <bkennedy@smm.org>
2015-10-08 09:53:45 -0500

    Updating URL and version
    This gets the download from the correct path for the new names and gets
    the latest version of the files.

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,8 +3,8 @@
 # $dmg_url - where to download the dmg
 # $app - location of installed application
 class seil::config {
-  $version = '11.0.0'
-  $base_url = 'https://pqrs.org/macosx/keyremap4macbook/files'
+  $version = '11.3.0'
+  $base_url = 'https://pqrs.org/osx/karabiner/files'
   $dmg_url = "${base_url}/Seil-${version}.dmg"
   $app = '/Applications/Seil.app'
   $cli = "${app}/Contents/Library/bin/seil"
